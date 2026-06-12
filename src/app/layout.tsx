d0ffa4c9c5f16ef3569714af5bc5dc58c4a4089e
Gurwinder Singh <60137236+GurwinderMankoo@users.noreply.github.com>
2026-06-12 13:03:39 +0530

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from './components/navbar/Main'
-import ModalProvider from './store/ModalProvider'
-import ScrollObserverProvider from './store/ScrollObserver'
-import ThemeSwitcher from './components/common/ThemeSwitcher'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/navbar/Main';
+import ModalProvider from './store/ModalProvider';
+import ScrollObserverProvider from './store/ScrollObserver';
+import ThemeSwitcher from './components/common/ThemeSwitcher';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
